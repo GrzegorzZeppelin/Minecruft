6b564529c16b309237dd5aa78a138f5866c52784
GrzegorzZeppelin <87579091+GrzegorzZeppelin@users.noreply.github.com>
2021-07-18 20:23:21 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let number = Math.floor(Math.random()*3+1);
 let photo = document.querySelector('.photo');
 
+let img = new Array();
+img[0] = '<img src="img/block1.jpg>';
+img[1] = '<img src="img/block2.jpg>';
+img[2] = '<img src="img/block3.jpg>';
+
 function photoChanging() {
 
     number++;
