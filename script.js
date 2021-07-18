d49de6b17a0437b37fcc3bf9b6f94595d7441bda
GrzegorzZeppelin <87579091+GrzegorzZeppelin@users.noreply.github.com>
2021-07-18 17:23:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// let photo = document.getElementById("photo");
+
+// let photoAmount = 3;
+// let number = Math.floor(Math.random()*photoAmount);
+
+// function photoChanging() {
+
+//     number++;
+//     if(number==photoAmount) {
+//         number = 1;
+//     }
+
+//     photo.innerHTML = '<img src="img/block1.jpg">'
+
+
+// }
+
+// window.addEventListener("load", photoChanging );
